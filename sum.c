38e64525e069e08895cf5249c85adcb9947c713b
Arickaji <66716199+Arickaji@users.noreply.github.com>
2020-09-30 20:00:45 +0530

    Update sum.c

--- sum.c
+++ sum.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 void main()
 {
-	int a,b,sum=0;
+	int a,b;
 	printf("Enter 1st number: \n");
 	scanf("%d",&a);
 	printf("Enter 2nd number: \n");
 	scanf("%d",&b);
-	sum=a+b;
-    printf("Sum=%d",sum);
+    	printf("Sum= %d + %d",a,b);
 }
