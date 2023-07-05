c4281554c2ebd930409ffbcfa8e96f7b232b864a
KAPIL SHARMA <98864374+kapilannu@users.noreply.github.com>
2023-07-05 12:41:24 +0530

    Update sum.c
    change variables

--- sum.c
+++ sum.c
@@ -3,9 +3,9 @@ void main()
 {
 	int a,b,sum=0;
 	printf("Enter 1st number: \n");
-	scanf("%d",&a);
-	printf("Enter 2nd number: \n");
 	scanf("%d",&b);
-	sum=a+b;
+	printf("Enter 2nd number: \n");
+	scanf("%d",&c);
+	sum=c+b;
     printf("Sum=%d",sum);
 }
